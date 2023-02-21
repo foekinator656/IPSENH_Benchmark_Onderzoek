d26c2c2caa7744aec57f601d73d382fb97ba6516
Loek Appel <loek@appel-snijder.nl>
2023-02-21 21:01:58 +0100

    update met time

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,17 @@ public class Main {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("uuuu/MM/dd HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
         System.out.println(dtf.format(now));
-        int count = 0;
-        while (count <= 1000){
-            ArrayGenerator arrayGenerator = new ArrayGenerator();
-            runSelectionSort(arrayGenerator);
-            runBubbleSort(arrayGenerator);
-            count++;
-        }
+
+        ArrayGenerator arrayGenerator = new ArrayGenerator();
+
+
+        runSelectionSort(arrayGenerator);
+        now = LocalDateTime.now();
+        System.out.println(dtf.format(now));
+
+        runBubbleSort(arrayGenerator);
+
+        now = LocalDateTime.now();
         System.out.println(dtf.format(now));
     }
 
@@ -20,39 +24,38 @@ public class Main {
         SelectionSort selectionSort = new SelectionSort();
 
         selectionSort.sort(arrayGenerator.hundredNumbersLongList);
-        selectionSort.printArray(arrayGenerator.hundredNumbersLongList);
+//        selectionSort.printArray(arrayGenerator.hundredNumbersLongList);
 
         selectionSort.sort(arrayGenerator.thousandNumbersLongList);
-        selectionSort.printArray(arrayGenerator.thousandNumbersLongList);
+//        selectionSort.printArray(arrayGenerator.thousandNumbersLongList);
 
         selectionSort.sort(arrayGenerator.tenThousandNumbersLongList);
-        selectionSort.printArray(arrayGenerator.tenThousandNumbersLongList);
+//        selectionSort.printArray(arrayGenerator.tenThousandNumbersLongList);
 
         selectionSort.sort(arrayGenerator.hundredThousandNumbersLongList);
-        selectionSort.printArray(arrayGenerator.hundredThousandNumbersLongList);
-
-        selectionSort.sort(arrayGenerator.thousandNumbersLongList);
-        selectionSort.printArray(arrayGenerator.thousandNumbersLongList);
-
-        selectionSort.sort(arrayGenerator.millionNumbersLongList);
-        selectionSort.printArray(arrayGenerator.millionNumbersLongList);
+//        selectionSort.printArray(arrayGenerator.hundredThousandNumbersLongList);
+//
+//        selectionSort.sort(arrayGenerator.millionNumbersLongList);
+////        selectionSort.printArray(arrayGenerator.millionNumbersLongList);
     }
 
     public static void runBubbleSort(ArrayGenerator arrayGenerator){
         BubbleSort bubbleSort = new BubbleSort();
 
         bubbleSort.bubbleSortWithSwapCounter(arrayGenerator.hundredNumbersLongList);
-        bubbleSort.printArray(arrayGenerator.hundredNumbersLongList);
+//        bubbleSort.printArray(arrayGenerator.hundredNumbersLongList);
 
         bubbleSort.bubbleSortWithSwapCounter(arrayGenerator.thousandNumbersLongList);
-        bubbleSort.printArray(arrayGenerator.thousandNumbersLongList);
+//        bubbleSort.printArray(arrayGenerator.thousandNumbersLongList);
 
         bubbleSort.bubbleSortWithSwapCounter(arrayGenerator.tenThousandNumbersLongList);
-        bubbleSort.printArray(arrayGenerator.tenThousandNumbersLongList);
+//        bubbleSort.printArray(arrayGenerator.tenThousandNumbersLongList);
 
-        bubbleSort.bubbleSortWithSwapCounter(arrayGenerator.tenThousandNumbersLongList);
-        bubbleSort.printArray(arrayGenerator.tenThousandNumbersLongList);
+        bubbleSort.bubbleSortWithSwapCounter(arrayGenerator.hundredThousandNumbersLongList);
+//        bubbleSort.printArray(arrayGenerator.tenThousandNumbersLongList);
 
+//        bubbleSort.bubbleSortWithSwapCounter(arrayGenerator.millionNumbersLongList);
+////        bubbleSort.printArray(arrayGenerator.millionNumbersLongList);
 
     }
 
