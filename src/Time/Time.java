0bdbb9da863491949bc332796e663f544fb8e7cb
Loek Appel <loek@appel-snijder.nl>
2023-03-06 22:39:38 +0100

    update van 6 maart 2023

--- src/Time/Time.java
+++ src/Time/Time.java
@@ -1,6 +1,7 @@
 package Time;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class Time {
 
@@ -25,22 +26,17 @@ public class Time {
     }
 
     public static String getTotalTimeNow(LocalDateTime start, LocalDateTime end){
-        int startHour = start.getHour();
-        int endHour = end.getHour();
-        int benchHour = endHour - startHour;
+        int hour = end.getHour() - start.getHour();
+        int minute = end.getMinute() - start.getMinute();
+        int second = end.getSecond() - start.getSecond();
 
-        int startMin = start.getMinute();
-        int endMin = end.getMinute();
-        int benchMin = endMin - startMin;
-        if (benchMin < 0) benchMin = (endMin + 60) - startMin;
+        if (minute < 0) minute = (end.getMinute() + 60) - start.getMinute();
+        if (second < 0) second = (end.getSecond() + 60) - start.getSecond();
+        System.out.println("The following int is being added to the list: " + second + "\n");
+        integers.add(second);
 
-        int startSec = start.getSecond();
-        int endSec = end.getSecond();
-        int benchSec = endSec - startSec;
-        if (benchSec < 0) benchSec = (endSec + 60) - startSec;
-
-        String totalTime = benchHour + " uur, " + benchMin + " min, " + benchSec + "sec";
-        System.out.println(totalTime);
+        String totalTime = String.format("%s uur, %s minuten, %s seconden", hour, minute, second);
+        System.out.println("Total time is: " + totalTime);
         return totalTime;
     }
 
